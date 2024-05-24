2a14f5fc0f7c9f1f51ea04ff25b520cc47a3030d
matiascrisnejo <matias.crisnejo@estudiantes.unahur.edu.ar>
2024-05-23 21:10:13 -0300

    pantalla agenda
    agregado de agenda falta adecuarlo ccon los detalles del auto

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { FilmsPage } from "./pages/FilmsPage";
 import { HomePage } from "./pages/HomePage";
+import Agenda from "./pages/Agenda"
+import Taller from "./pages/Taller"
+import Estadisticas from "./pages/Estadisticas"
+import RegistrarPago from "./pages/RegistrarPago"
+import Login from "./pages/Login"
 import { ListOfFilms } from "./pages/FilmsPageReduxStyle";
 import { ActorsPage } from "./pages/ActorsPage";
 import { Box } from "@mui/material";
@@ -12,6 +17,15 @@ export function AppRouter() {
       <Route path='/reserve/*' element={<ActorsPage />} />
       <Route path='/home' element={<HomePage />} />
       <Route path='/' element={<HomePage />} />
+      
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/taller' element={<Taller />} />
+      <Route path='/agenda' element={<Agenda />} />
+      <Route path='/estadisticas' element={<Estadisticas />} />
+      <Route path='/registrarPago' element={<RegistrarPago />} />
+      <Route path='/login' element={<Login />} />
+      
+
     </Routes>
   );
 }
