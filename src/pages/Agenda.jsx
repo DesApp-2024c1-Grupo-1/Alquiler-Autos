fee53ed4575f9e1d8255a3d5f893926f9fb704d0
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2024-06-22 17:23:39 -0300

    datos para back falso de agenda

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -13,18 +13,72 @@ function App() {
  
 let eventosDePrueba = [
   {
-    "start": "2024-06-22T06:00:00.000Z",
-    "end": "2024-06-22T07:00:00.000Z",
-    "text": "PATENTE JMD 870",
-    "color": "#f67944"
+    "start": "2024-06-25T10:00:00.000Z",
+    "end": "2024-06-25T10:15:00.000Z",
+    "text": "Alquiler: entrega AHC143",
+    "color": "#ff0000"
   },
   {
-    "start": "2024-06-23T06:00:00.000Z",
-    "end": "2024-06-23T06:15:00.000Z",
-    "text": "Green box to post office",
-    "color": "#6e7f29"
+    "start": "2024-06-25T14:00:00.000Z",
+    "end": "2024-06-25T14:15:00.000Z",
+    "text": "Alquiler: entrega DEF456",
+    "color": "#ff0000"
   },
-];
+  {
+    "start": "2024-06-26T12:00:00.000Z",
+    "end": "2024-06-26T12:15:00.000Z",
+    "text": "Alquiler: devolución DEF456",
+    "color": "#00ff00"
+  },
+  {
+    "start": "2024-06-27T10:00:00.000Z",
+    "end": "2024-06-27T10:15:00.000Z",
+    "text": "Alquiler: entrega JKL012",
+    "color": "#ff0000"
+  },
+  {
+    "start": "2024-06-27T14:00:00.000Z",
+    "end": "2024-06-27T14:15:00.000Z",
+    "text": "Alquiler: devolución AHC143",
+    "color": "#00ff00"
+  },
+  {
+    "start": "2024-06-28T16:00:00.000Z",
+    "end": "2024-06-28T16:15:00.000Z",
+    "text": "Alquiler: entrega MNO345",
+    "color": "#ff0000"
+  },
+  {
+    "start": "2024-06-29T10:00:00.000Z",
+    "end": "2024-06-29T10:15:00.000Z",
+    "text": "Alquiler: devolución JKL012",
+    "color": "#00ff00"
+  },
+  {
+    "start": "2024-06-29T18:00:00.000Z",
+    "end": "2024-06-29T18:15:00.000Z",
+    "text": "Alquiler: entrega STU901",
+    "color": "#ff0000"
+  },
+  {
+    "start": "2024-06-30T14:00:00.000Z",
+    "end": "2024-06-30T14:15:00.000Z",
+    "text": "Alquiler: devolución MNO345",
+    "color": "#00ff00"
+  },
+  {
+    "start": "2024-06-30T20:00:00.000Z",
+    "end": "2024-06-30T20:15:00.000Z",
+    "text": "Alquiler: entrega PQR678",
+    "color": "#ff0000"
+  },
+  {
+    "start": "2024-07-01T10:00:00.000Z",
+    "end": "2024-07-01T10:15:00.000Z",
+    "text": "Alquiler: devolución STU901",
+    "color": "#00ff00"
+  }
+]
 
   const [myEvents, setEvents] = useState([]);
   const [isToastOpen, setToastOpen] = useState(false);
