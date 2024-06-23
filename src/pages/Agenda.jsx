d11a4b7b03c733c8ae5addd5360ac67a68e08d8a
Pablo <pablo_lopesferrao@hotmail.com>
2024-06-23 01:40:12 -0300

    Agregando Funcionalidades a Agenda
    Agregando Hover, etc

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,6 +1,6 @@
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
-import { Eventcalendar, getJson, setOptions, Toast, localeEs } from '@mobiscroll/react';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { Button, Eventcalendar, formatDate, Popup, setOptions, Toast, localeEs } from '@mobiscroll/react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 
 setOptions({
   locale: localeEs,
@@ -8,9 +8,8 @@ setOptions({
   themeVariant: 'light'
 });
 
-function App() {
 
- 
+/*
 let eventosDePrueba = [
   {
     "start": "2024-06-25T10:00:00.000Z",
@@ -80,46 +79,258 @@ let eventosDePrueba = [
   }
 ]
 
-  const [myEvents, setEvents] = useState([]);
+*/
+
+/*Ver si eliminar boton confirm/cancel, que cambia el estado.
+  Ver si prefieren que vuelva a aparecer cuando se devuelve, o solo que aparezca una vez.
+*/
+
+const eventosDePrueba = [
+  {
+    title: 'JUD-123',
+    nombreYApellido: 'Jesus Albornoz',
+    age: 68,
+    start: '2024-06-25T08:00',
+    end: '2024-06-26T08:45',
+    confirmed: false,
+    lugarRetiro: 'Calle Falsa 123, Hurlingham',
+    lugarDevolucion: 'Calle Verdadera 456, Hurlingham',
+    color: '#00ff00',
+  },
+  {
+    title: 'LEO-789',
+    nombreYApellido: 'Pedro Saborido',
+    age: 44,
+    start: '2024-06-26T09:00',
+    end: '2024-06-28T09:45',
+    confirmed: false,
+    lugarRetiro: 'Mazza 1780, Moron',
+    lugarDevolucion: 'Pola 2600, Moron',
+    color: '#ff0000',
+  },
+  {
+    title: 'MER-456',
+    nombreYApellido: 'Juan Cruz Mendoza',
+    age: 29,
+    start: '2024-06-28T10:00',
+    end: '2024-06-29T10:45',
+    confirmed: true,
+    lugarRetiro: 'Libertad 3100, Moron',
+    lugarDevolucion: 'Paz 1800, Hurlingham',
+    color: '#b33d3d',
+  },
+
+  {
+    title: 'DER-987',
+    nombreYApellido: 'Juan Maldonado',
+    age: 72,
+    start: '2024-06-27T13:00',
+    end: '2024-06-30T13:45',
+    confirmed: false,
+    lugarRetiro: 'Avenida Siempre Viva 600, Haedo',
+    lugarDevolucion: 'Avenida de los libertadores 300, Haedo',
+    color: '#b33d3d',
+  },
+  {
+    title: 'JEN-654',
+    nombreYApellido: 'Juan Maldonado',
+    age: 65,
+    start: '2024-06-30T10:00',
+    end: '2024-06-30T14:45',
+    confirmed: true,
+    lugarRetiro: 'Avenida Pierrastegui 4100, Moron',
+    lugarDevolucion: 'Juana Azurduy 1680, Hurlingham',
+    color: '#b33d3d',
+  },
+  {
+    title: 'LIL-321',
+    nombreYApellido: 'Juan Maldonado',
+    age: 54,
+    start: '2024-06-30T10:00',
+    end: '2024-07-1T10:45',
+    confirmed: true,
+    lugarRetiro: '25 de Mayo 1470, Moron',
+    lugarDevolucion: 'Procare 2500, Hurlingham ',
+    color: '#309346',
+  },
+  {
+    title: 'HCK-780',
+    nombreYApellido: 'Pedro Rosemblant',
+    age: 59,
+    start: '2024-06-25T11:00',
+    end: '2024-06-30T11:45',
+    confirmed: true,
+    lugarRetiro: 'Avenida Eva Peron 1790, Moron',
+    lugarDevolucion: 'Avenida Independencia, Hurlingham',
+    color: '#309346',
+  },
+];
+
+
+function App() {
+  const [appointments, setAppointments] = useState(eventosDePrueba);
+  const [appointment, setAppointment] = useState();
+  const [appointmentInfo, setAppointmentInfo] = useState('');
+  const [appointmentLocation, setAppointmentLocation] = useState('');
+  const [appointmentReason, setAppointmentReason] = useState('');
+  const [appointmentStatus, setAppointmentStatus] = useState('');
+  const [appointmentTime, setAppointmentTime] = useState('');
+  const [buttonText, setButtonText] = useState('');
+  const [buttonType, setButtonType] = useState('');
+  const [isTooltipOpen, setTooltipOpen] = useState(false);
   const [isToastOpen, setToastOpen] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
+  const [tooltipAnchor, setTooltipAnchor] = useState(null);
+  const [tooltipColor, setTooltipColor] = useState('');
 
-  const handleCloseToast = useCallback(() => {
+  const timer = useRef(null);
+
+  const myView = useMemo(() => ({ agenda: { type: 'week' } }), []);
+
+  const openTooltip = useCallback((args) => {
+    const event = args.event;
+    /* Muestra el tiempo cuando te apoyas sobre alguna patente*/
+    const time = formatDate('DD MMM YYYY', new Date(event.start)) + ' - ' + formatDate('DD MMM YYYY', new Date(event.end));
+
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+
+    /*Este sirve para modificar lo que sale cuando me posicion con el mouse*/
+    if (event.confirmed) {
+      setAppointmentStatus('Reservado');
+      setButtonText('Cancel appointment');
+      setButtonType('warning');
+    } else {
+      setAppointmentStatus('Devuelto');
+      setButtonText('Confirm appointment');
+      setButtonType('success');
+    }
+
+    /* Este es cuando hago el hover*/
+    setAppointment(event);
+    /*Borro el age despues me fijo si ponerlo otra vez o no, lo quito porque el formatDate no queda sino
+    setAppointmentInfo(event.nombreYApellido + ', Age: ' + event.age);*/
+    setAppointmentInfo(event.nombreYApellido);
+    setAppointmentLocation(event.lugarDevolucion);
+    setAppointmentTime(time);
+    setAppointmentReason(event.lugarRetiro);
+    setTooltipColor(event.color);
+    setTooltipAnchor(args.domEvent.target);
+    setTooltipOpen(true);
+  }, []);
+
+  const handleEventHoverIn = useCallback(
+    (args) => {
+      openTooltip(args);
+    },
+    [openTooltip],
+  );
+
+  const handleEventHoverOut = useCallback(() => {
+    if (!timer.current) {
+      timer.current = setTimeout(() => {
+        setTooltipOpen(false);
+      }, 200);
+    }
+  }, []);
+
+  const handleEventClick = useCallback(
+    (args) => {
+      openTooltip(args);
+    },
+    [openTooltip],
+  );
+
+  const handleMouseEnter = useCallback(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = null;
+    }
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    timer.current = setTimeout(() => {
+      setTooltipOpen(false);
+    }, 200);
+  }, []);
+
+  const handleToastClose = useCallback(() => {
     setToastOpen(false);
   }, []);
 
-  const handleEventClick = useCallback((args) => {
-    setToastMessage(args.event.title);
+  /*Este const sirve para cuando yo hago click sobre el cancelar*/ 
+  const updateAppointmentStatus = useCallback(() => {
+    appointment.confirmed = !appointment.confirmed;
+    setTooltipOpen(false);
+    setToastMessage('Auto ' + (appointment.confirmed ? 'confirmado' : 'cancelado'));
+    setToastOpen(true);
+  }, [appointment]);
+
+  const viewAppointmentFile = useCallback(() => {
+    setTooltipOpen(false);
+    setToastMessage('View file');
     setToastOpen(true);
   }, []);
 
-  const myView = useMemo(
-    () => ({
-      calendar: { type: 'week' },
-      agenda: { type: 'day' },
-    }),
-    [],
-  );
-
-  
-
-  useEffect(() => {
-    getJson
-        setEvents(eventosDePrueba);
-  },[]);
+  /* Este const sirve para cuando yo hago click sobre eliminar*/
+  const deleteAppointment = useCallback(() => {
+    setAppointments(appointments.filter((item) => item.id !== appointment.id));
+    setTooltipOpen(false);
+    setToastMessage('Auto eliminado de agenda');
+    setToastOpen(true);
+  }, [appointments, appointment]);
 
   return (
     <>
-     <Eventcalendar
-  data={myEvents}
-  view={myView}
-  onEventClick={handleEventClick}
-  theme="ios" // Personaliza el tema (opcional)
-  themeVariant="light" // Personaliza la variante del tema (opcional)
-/>
-
-
-      <Toast message={toastMessage} isOpen={isToastOpen} onClose={handleCloseToast} />
+      <Eventcalendar
+        data={appointments}
+        showEventTooltip={false}
+        view={myView}
+        onEventHoverIn={handleEventHoverIn}
+        onEventHoverOut={handleEventHoverOut}
+        onEventClick={handleEventClick}
+      />
+      <Popup
+        anchor={tooltipAnchor}
+        closeOnOverlayClick={false}
+        contentPadding={false}
+        display="anchored"
+        isOpen={isTooltipOpen}
+        showOverlay={false}
+        touchUi={false}
+        width={350}
+      >
+        <div className="mds-tooltip" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <div className="mds-tooltip-header" style={{ backgroundColor: tooltipColor }}>
+            <span>{appointmentInfo}</span>
+            <span className="mbsc-pull-right">{appointmentTime}</span>
+          </div>
+          <div className="mbsc-padding">
+            <div className="mds-tooltip-label mbsc-margin">
+              Estado: <span className="mbsc-light">{appointmentStatus}</span>
+              <Button color={buttonType} variant="outline" className="mds-tooltip-button mbsc-pull-right" onClick={updateAppointmentStatus}>
+                {buttonText}
+              </Button>
+            </div>
+            
+            <div className="mds-tooltip-label mbsc-margin">
+              Lugar Retiro: <span className="mbsc-light">{appointmentReason}</span>
+            </div>
+            
+            <div className="mds-tooltip-label mbsc-margin">
+              Lugar Devolucion: <span className="mbsc-light">{appointmentLocation}</span>
+            </div>
+            <Button color="secondary" className="mds-tooltip-button" onClick={viewAppointmentFile}>
+              Datos de Cliente
+            </Button>
+            <Button color="danger" variant="outline" className="mds-tooltip-button mbsc-pull-right" onClick={deleteAppointment}>
+              Eliminar
+            </Button>
+          </div>
+        </div>
+      </Popup>
+      <Toast isOpen={isToastOpen} message={toastMessage} onClose={handleToastClose} />
     </>
   );
 }
