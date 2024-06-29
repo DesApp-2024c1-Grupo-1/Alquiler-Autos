53ba2a0dc6ea3ebcf2393c541fe60da4c348623b
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2024-06-29 19:35:23 -0300

    armando estructura

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -15,6 +15,27 @@ export function Estadisticas() {
     fetchAllAlquileres();
     }, [fetchAllAlquileres]);
 
+
+/*
+
+id	
+fechaRetiro	
+lugarRetiro	
+fechaDevolucion	
+lugarDevolucion	
+precioFinal	
+cantidadDias	
+carId	
+clienteId	
+
+const alquileresTotales = allAlquileres.reduce((prev, current) => {
+  return (prev.vecesAlquilado > current.vecesAlquilado) ? prev : current;
+}, {});
+    
+*/
+
+
+
     const estilo = {backgroundColor: "#e4e9f0", borderRadius: '5px', border: "solid", padding: 2}
     
     return (
@@ -23,24 +44,29 @@ export function Estadisticas() {
                 <Grid item xs={12} sm={6} md={4} lg={4} sx={estilo}>
                     <h2>Alquileres Del Mes</h2>
                     <hr style={{ border: '1px solid'}} />
-                    (
-                        <div>
+                    <div>
                         <p>Cantidad de Alquileres: </p>
                         <p>Días totales de alquiler: </p>
                         <p>Ganancia total:</p>
-                        </div>
-                    )
+                    </div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4} lg={4} sx={estilo}>
-                    <h2>Alquileres del año</h2>
+                    <h2>Alquileres Del Año</h2>
                     <hr style={{ border: '1px solid'}} />
-                    (
-                        <div>
+                    <div>
                         <p>Cantidad de Alquileres: </p>
                         <p>Días totales de alquiler: </p>
                         <p>Ganancia total:</p>
-                        </div>
-                    )
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={4} sx={estilo}>
+                    <h2>Alquileres, todos</h2>
+                    <hr style={{ border: '1px solid'}} />
+                    <div>
+                        <p>Cantidad de Alquileres: </p>
+                        <p>Días totales de alquiler: </p>
+                        <p>Ganancia total:</p>
+                    </div>
                 </Grid>
                 
             </Grid>
--- src/services/Estadisticas.js
+++ src/services/Estadisticas.js
@@ -12,38 +12,3 @@ export async function getAllAlquileres() {
       console.error(error);
     }
 };
-    
-/*
-@Entity()
-export class Alquiler {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
-
-    @Column()
-    fechaRetiro: Date;
-
-    @Column()
-    lugarRetiro: string;
-
-    @Column()
-    fechaDevolucion: Date;
-
-    @Column()
-    lugarDevolucion: string;
-
-    @Column()
-    precioFinal: number;
-
-    @Column()
-    cantidadDias: number;
-
-    @ManyToOne(() => Car, car => car.alquiler)
-    car: Car;
-
-    @ManyToOne(() => Cliente, cliente => cliente.alquiler,{cascade: true})
-    cliente: Cliente;
-} 
-
-
-
-*/
