62d4013ac61fae91d9205504f8a9e5a337de2e76
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2024-10-14 16:47:58 -0300

    mejora estetica

--- src/pages/Alquileres.jsx
+++ src/pages/Alquileres.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText,  } from '@mui/material';
-
+import { Box, Card, CardContent, Typography, List } from '@mui/material';
 export function AlquileresPage() {
 
     const [allAlquileres, setAllAlquileres] = useState([]);
@@ -11,6 +10,8 @@ export function AlquileresPage() {
             const response = await axios.get("http://localhost:3000/alquiler");
             const alquileres = response.data;
             console.log("Alquileres obtenidos:", alquileres);
+             // Ordenar los alquileres por fechaRetiro (de más antigua a más reciente)
+            alquileres = alquileres.sort((a, b) => new Date(a.fechaRetiro) - new Date(b.fechaRetiro));
             setAllAlquileres(alquileres);
         } catch (error) {
             console.error("Error fetching alquileres:", error);
@@ -24,19 +25,32 @@ export function AlquileresPage() {
 
     return (
         <>
-            <h1>Alquileres</h1>
-            <nav>
-                <List>
-                    {
-                        allAlquileres.map((alquiler) => (
-                            <h1>id de alquiler: {alquiler.id}, fecha de inicio alquiler: {alquiler.fechaRetiro} </h1>
-                        ))
-                    }
-                </List>
-            </nav>
+            <Typography variant="h4" gutterBottom>
+                Alquileres
+            </Typography>
+                {allAlquileres.map((alquiler) => (
+                    <Card 
+                        key={alquiler.id} 
+                        sx={{
+                            mb: 2,
+                            borderRadius: '16px',
+                            backgroundColor: '#B3D0FB', // Fondo celeste claro
+                            boxShadow: 3
+                        }}
+                    >
+                        <CardContent>
+                            <Typography variant="h6">
+                                ID de Alquiler: {alquiler.id}
+                            </Typography>
+                            <Typography variant="body1">
+                                Fecha de Inicio: {alquiler.fechaRetiro}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                ))}
         </>
-            );
-        }
+    );
+}
 
 export default AlquileresPage;
 
