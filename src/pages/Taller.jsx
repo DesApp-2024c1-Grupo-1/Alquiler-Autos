3043a5714d46df470faf6bf33df247a7de8cb73d
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2024-11-14 20:43:07 -0300

    mejoras en taller

--- src/pages/Taller.jsx
+++ src/pages/Taller.jsx
@@ -13,26 +13,52 @@ function Taller() {
         });
     }, []); // Ejecutar solo al montar el componente
 
+    const toggleReparacion = (index) => {
+        setCars((prevCars) =>
+            prevCars.map((car, i) =>
+                i === index
+                    ? { ...car, enReparacion: car.enReparacion === null || car.enReparacion === false ? true : false }
+                    : car
+            )
+        );
+    };
+
     return (
         <>
             <h1>Taller</h1>
             <div>
-                {cars.map((car, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            border: "1px solid #ccc",
-                            padding: "10px",
-                            margin: "10px 0",
-                            borderRadius: "5px",
-                        }}
-                    >
+            {cars.map((car, index) => (
+                <div
+                    key={index}
+                    style={{
+                        display: "flex", // Flex para organizar en filas
+                        alignItems: "center", // Centrar verticalmente los elementos
+                        border: "1px solid #ccc",
+                        padding: "10px",
+                        margin: "10px 0",
+                        borderRadius: "5px",
+                    }}
+                >
+                    <img 
+                        src={car.image} 
+                        alt={`${car.name} image`} 
+                        style={{ width: "150px", height: "auto", borderRadius: "5px", marginRight: "10px" }} 
+                    />
+                    <div>
                         <h2>{car.name}</h2>
                         <p>Marca: {car.brand}</p>
                         <p>Año: {car.year}</p>
+                        <p>Reparación Pendiente: {car.enReparacion ? 'si': 'no'}</p>
                     </div>
+                    <button onClick={() => toggleReparacion(index)}>
+                            Cambiar Estado
+                    </button>
+                    <button>
+                            Asignar evento reparacion, no anda
+                    </button>
+                </div>
                 ))}
-            </div>
+                </div>
         </>
     );
 }
