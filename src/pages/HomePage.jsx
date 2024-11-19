21df3827ba75b728d58953bd6e56812df888f050
marcosmacagna <marcos.macagna@estudiantes.unahur.edu.ar>
2024-11-19 20:12:54 -0300

    Agregado boton para volver hacia arriba, en Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Fab, Tooltip } from "@mui/material";
+import { KeyboardArrowUp } from '@mui/icons-material'; //Icono para el boton
 import { CarCard } from '../components/CarCard.jsx';
 import { getAllCarsAvailable } from "../services/CarsService";
 import Filtros from '../components/Filtros.jsx';
@@ -18,6 +19,22 @@ export function HomePage() {
     //const obtainedCars = await getAllCarsFake();
     setAllCars(obtainedCars);
   }, []);
+
+  const [showScrollButton, setShowScrollButton] = useState(false); //Estado para controlar la visibilidad del botón
+
+  //Effect para controlar la visibilidad del botón de scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollButton(window.scrollY > 200);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //Función para hacer scroll hacia arriba
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   const handleFiltros = (filtros) => {
     console.log("Filtros en handleFiltros: ", filtros)
@@ -59,6 +76,30 @@ export function HomePage() {
 
             {/* <Buscador sx={{ mx: 20, my: 20 }} /> */}
             <ButtonAddCar setAllCars={setAllCars} allCars={allCars} />
+            
+            {/* Botón para hacer scroll hacia arriba con Tooltip */}
+            {showScrollButton && (
+                  <Tooltip 
+                    title={<span style={{ fontSize: '1.2rem' }}>Volver arriba</span>} //Tamaño de la leyenda
+                    placement="left" 
+                    arrow
+                  >
+                    <Fab
+                      color="primary"
+                      onClick={scrollToTop}
+                      sx={{
+                        width: 80, //Ancho del botón
+                        height: 80, //Alto del botón
+                        position: 'fixed',
+                        bottom: 16,
+                        right: 16,
+                        zIndex: 1000,
+                      }}
+                    >
+                      <KeyboardArrowUp sx={{ fontSize: '2rem' }} /> {/* Tamaño del ícono */}
+                    </Fab>
+                  </Tooltip>
+            )}
 
             <Grid container spacing={2} sx={{ mt: '1rem' }}>
               {allCars && allCars.length > 0 ? (
