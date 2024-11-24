44579f87e8ae6c8482cd3814921ec3ba70711a08
matiascrisnejo <matias.crisnejo@estudiantes.unahur.edu.ar>
2024-11-24 19:46:18 -0300

    skeleton en taller

--- src/pages/Taller.jsx
+++ src/pages/Taller.jsx
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { 
     Button, Typography, Box, Grid, Dialog, DialogTitle, DialogContent, 
-    DialogActions, TextField 
+    DialogActions, TextField, Skeleton 
 } from '@mui/material';
 import { getAllCarsAvailable } from "../services/CarsService";
-import {registrarReparacion} from "../services/tallerService";
+import { registrarReparacion } from "../services/tallerService";
 import faviconTaller from '../assets/faviconTaller.png';
 
 function Taller() {
     const [cars, setCars] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // Estado para manejar el skeleton de carga
     const [openDialog, setOpenDialog] = useState(false);
     const [selectedCar, setSelectedCar] = useState(null);
     const [entryDate, setEntryDate] = useState('');
     const [exitDate, setExitDate] = useState('');
     const [razon, setRazon] = useState('');
 
-    // Obtener la fecha actual en formato YYYY-MM-DD
     const getTodayDate = () => {
         const today = new Date();
         const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0'); // Mes comienza desde 0
+        const month = String(today.getMonth() + 1).padStart(2, '0');
         const day = String(today.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     };
 
     const todayDate = getTodayDate();
 
-    //Icono de la página en la pestaña del navegador.
     useEffect(() => {
         const favicon = document.querySelector('link[rel="icon"]') || document.createElement('link');
         favicon.rel = 'icon';
@@ -43,9 +42,19 @@ function Taller() {
             .then((carsData) => {
                 console.log("Autos filtrados y disponibles: ", carsData);
                 setCars(carsData);
+
+                // Retrasar la visualización de los datos por 3 segundos
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 3000);
             })
             .catch((error) => {
                 console.error("Error al obtener los autos:", error);
+
+                // Manejar error y ocultar skeleton tras 3 segundos
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 3000);
             });
     }, []);
 
@@ -94,57 +103,75 @@ function Taller() {
                 Taller
             </Typography>
             <Grid container spacing={2}>
-                {cars.map((car, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: { xs: 'column', md: 'row' },
-                                alignItems: 'center',
-                                border: '1px solid #ccc',
-                                padding: 2,
-                                borderRadius: 1,
-                                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                            }}
-                        >
-                            <img
-                                src={car.image}
-                                alt={`${car.name} image`}
-                                style={{
-                                    width: '100%',
-                                    maxWidth: '150px',
-                                    height: 'auto',
-                                    borderRadius: '5px',
-                                    marginBottom: '10px',
+                {isLoading ? (
+                    // Mostrar Skeleton mientras se cargan los autos
+                    Array.from({ length: 6 }).map((_, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Box
+                                sx={{
+                                    border: '1px solid #ccc',
+                                    padding: 2,
+                                    borderRadius: 1,
+                                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
                                 }}
-                            />
-                            <Box sx={{ flex: 1, marginLeft: { md: 2 } }}>
-                                <Typography variant="h6">{car.name}</Typography>
-                                <Typography>Patente: {car.patente}</Typography>
-                                <Typography>Año: {car.year}</Typography>
-                                {/* <Typography>
-                                    Reparación Pendiente: {car.enReparacion ? 'Sí' : 'No'}
-                                </Typography> */}
+                            >
+                                <Skeleton variant="rectangular" height={150} sx={{ borderRadius: 1 }} />
+                                <Skeleton variant="text" sx={{ mt: 2 }} width="80%" />
+                                <Skeleton variant="text" width="60%" />
+                                <Skeleton variant="rounded" sx={{ mt: 2 }} width="50%" height={30} />
                             </Box>
+                        </Grid>
+                    ))
+                ) : (
+                    cars.map((car, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
                             <Box
                                 sx={{
                                     display: 'flex',
-                                    flexDirection: 'column',
-                                    gap: 1,
-                                    marginTop: { xs: 2, md: 0 },
+                                    flexDirection: { xs: 'column', md: 'row' },
+                                    alignItems: 'center',
+                                    border: '1px solid #ccc',
+                                    padding: 2,
+                                    borderRadius: 1,
+                                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
                                 }}
                             >
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={() => openPopup(car)}
+                                <img
+                                    src={car.image}
+                                    alt={`${car.name} image`}
+                                    style={{
+                                        width: '100%',
+                                        maxWidth: '150px',
+                                        height: 'auto',
+                                        borderRadius: '5px',
+                                        marginBottom: '10px',
+                                    }}
+                                />
+                                <Box sx={{ flex: 1, marginLeft: { md: 2 } }}>
+                                    <Typography variant="h6">{car.name}</Typography>
+                                    <Typography>Patente: {car.patente}</Typography>
+                                    <Typography>Año: {car.year}</Typography>
+                                </Box>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        gap: 1,
+                                        marginTop: { xs: 2, md: 0 },
+                                    }}
                                 >
-                                    Taller
-                                </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => openPopup(car)}
+                                    >
+                                        Taller
+                                    </Button>
+                                </Box>
                             </Box>
-                        </Box>
-                    </Grid>
-                ))}
+                        </Grid>
+                    ))
+                )}
             </Grid>
 
             <Dialog open={openDialog} onClose={closePopup} maxWidth="sm" fullWidth>
@@ -158,7 +185,7 @@ function Taller() {
                         onChange={(e) => setEntryDate(e.target.value)}
                         sx={{ mb: 2, mt: 2 }}
                         InputLabelProps={{ shrink: true }}
-                        inputProps={{ min: todayDate }} // Validación para no permitir fechas pasadas
+                        inputProps={{ min: todayDate }}
                     />
                     <TextField
                         fullWidth
@@ -167,7 +194,7 @@ function Taller() {
                         value={exitDate}
                         onChange={(e) => setExitDate(e.target.value)}
                         InputLabelProps={{ shrink: true }}
-                        inputProps={{ min: entryDate || todayDate }} // No permitir fechas antes de la fecha de entrada
+                        inputProps={{ min: entryDate || todayDate }}
                     />
                     <TextField
                         fullWidth
@@ -177,7 +204,7 @@ function Taller() {
                         multiline
                         rows={3}
                         placeholder="Escribe la razón del mantenimiento..."
-                        sx={{ mb: 2 ,mt:2}}
+                        sx={{ mb: 2, mt: 2 }}
                     />
                 </DialogContent>
                 <DialogActions>
